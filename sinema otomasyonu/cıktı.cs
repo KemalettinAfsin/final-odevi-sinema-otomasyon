d8c7c31486ebd91c503f2210bc2fa3a5a74166a8
KemalettinAfsin <122862644+KemalettinAfsin@users.noreply.github.com>
2023-06-16 13:50:29 +0300

    sinema otomoasyonu
    final ödevi

---
+++ sinema otomasyonu/cıktı.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinema_otomasyonu
+{
+    class cıktı
+    {
+            static void main()
+            {
+                PrintDocument PD = new PrintDocument();
+                PD.PrintPage += new PrintPageEventHandler(OnPrintDocument);
+
+                try
+                {
+                    PD.Print();
+                }
+                catch
+                {
+                    Console.WriteLine("Yazıcı çıktısı alınamıyor...");
+                }
+                finally
+                {
+                    PD.Dispose();
+                }
+            }
+
+            private static void OnPrintDocument(object sender, PrintPageEventArgs e)
+            {
+                e.Graphics.DrawRectangle(Pens.Red, 20, 20, 100, 100);
+            }
+        }
+}
